8a4a0f478e50303ab5e29c63c11e5ec0e5d47971
jaakko <jaakko.jokinen@outlook.com>
2015-09-27 23:16:44 +0300

    change: Settings changes

--- server/opiskelijabileet/settings.py
+++ server/opiskelijabileet/settings.py
@@ -23,9 +23,13 @@ BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
 SECRET_KEY = 'wqtd@=%^urzr=34&4-*ti0ej7c=il5zmyi&&6a9ug^7dkes6!w'
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = True
+DEBUG = False
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = [
+    'localhost',
+    '127.0.0.1',
+    '.opiskelijatapahtumat.fi'
+]
 
 
 # Application definition
