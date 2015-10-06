0d968c50964a15a157ce32f39e447dcc7885f6a5
jaakko <jaakko.jokinen@outlook.com>
2015-10-06 20:56:32 +0300

    add: Permissions for create view

--- server/event/urls.py
+++ server/event/urls.py
@@ -6,7 +6,7 @@ urlpatterns = [
     url(r'^query$', views.DateQuery.as_view()),
     url(r'^as-dates/(?P<year>[0-9]{4})/(?P<month>[0-9]+)/(?P<day>[0-9]+)$', views.DateList.as_view()),
     url(r'^as-dates/(?P<year>[0-9]{4})/(?P<month>[0-9]{2})/(?P<day>[0-9]+)/(?P<daterange>[0-9]+)$', views.DateRangeList.as_view()),
-    url(r'^event/$', views.EventSingle.as_view()),
+    url(r'^event/$', views.create_event),
     url(r'^event/(?P<id>[0-9]+)/$', views.EventSingle.as_view()),
 ]
 
